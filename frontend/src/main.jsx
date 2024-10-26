fb93eac08ec2f61f41b936ef161f3b034efd6029
MUHANNADWA <muhannadw2005@gmail.com>
2024-10-26 16:59:13 +0300

    1- ADDED PRODUCT, HEADER AND FOOTER COMPONENTS. 2- ADDED HOMESCREEN VIEW 3- ADDED IMAGES IN ASSETS. 4- ADDED CUSTOM BOOTSTRAP THEME 5- ADDED PRODUCT.JS FILE TEMPORARILY

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App.jsx'
-import './index.css'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
