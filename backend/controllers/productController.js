db30dd6e3df303db3ac6d0ce9388fdbc76051d5c
MUHANNADWA <muhannadw2005@gmail.com>
2024-10-27 14:56:50 +0300

    added controller to clear router

---
+++ backend/controllers/productController.js
@@ -0,0 +1,67 @@
+import express from "express";
+import productModel from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc    Fetch all products
+// @route   GET /products
+// @access  Public
+const getProducts = async (req, res) => {
+    try {
+        const products = await productModel.find();
+        res.status(200).send(products);
+    } catch (error) {
+        res.status(500).send(`Something went wrong ${error}`);
+    }
+}
+
+// @desc    Fetch single product
+// @route   GET /products/:id
+// @access  Public
+const getProductById = async (req, res) => {
+    const id = req.params.id
+
+    const product = await productModel.findById(id);
+    if (!product) {
+        res.status(404).send("product not found");
+    }
+    else res.status(200).send(product);
+}
+
+
+const addProduct = async (req, res) => {
+    try {
+        const data = req.body;
+
+        const newProduct = await productModel.create(data);
+        newProduct.save();
+        res.status(201).send(newProduct);
+    } catch (error) {
+        res.status(500).send(`Something went wrong ${error}`);
+    }
+}
+
+const updateProduct = async (req, res) => {
+    const id = req.params.id
+    const data = req.body;
+
+    const product = await productModel.findByIdAndUpdate(id, data, { new: true });
+    if (!product) {
+        res.status(404).send("product not found");
+    }
+    else res.status(204).send(product);
+}
+
+const deleteProduct = async (req, res) => {
+    const id = req.params.id
+
+    const product = await productModel.findByIdAndDelete(id);
+    if (!product) {
+        res.status(404).send("product not found");
+    }
+    else res.send("deleted");
+}
+
+export { getProducts, getProductById, addProduct, deleteProduct, updateProduct };
+
+export default router;
