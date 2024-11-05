efafece7374772f74d51cbbd4528ee8d679e5677
MUHANNADWA <muhannadw2005@gmail.com>
2024-11-05 15:02:25 +0300

    rename router to routes "Files"

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
-import productsRouter from './routers/productsRouter.js'
-import usersRouter from './routers/usersRouter.js'
-import ordersRouter from './routers/ordersRouter.js';
+import productsRoutes from './routes/productsRoutes.js'
+import usersRoutes from './routes/usersRoutes.js'
+import ordersRoutes from './routes/ordersRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { ORDERS_URL, PRODUCTS_URL, USERS_URL } from '../frontend/src/constants.js';
 
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.use(PRODUCTS_URL, productsRouter);
-app.use(USERS_URL, usersRouter);
-app.use(ORDERS_URL, ordersRouter);
+app.use(PRODUCTS_URL, productsRoutes);
+app.use(USERS_URL, usersRoutes);
+app.use(ORDERS_URL, ordersRoutes);
 
 app.get("/", (req, res) => {
     res.send("Running, Enter to <a href=http://localhost:5000/products>http://localhost:5000/products</a>");
