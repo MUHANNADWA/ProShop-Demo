160d1d93303a7d38341f611714731740fe575cd0
MUHANNADWA <muhannadw2005@gmail.com>
2024-11-10 16:48:18 +0300

    changed backend url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:10000";
+export const BASE_URL = "https://proshop-backend-p8js.onrender.com";
 export const PRODUCTS_URL = "/products";
 export const ORDERS_URL = "/orders";
 export const USERS_URL = "/users";
